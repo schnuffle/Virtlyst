9b2318b5bbd849c7c6571a526dd9d776cbb14658
Schnuffle <schnuffle@gotec-cafe.de>
2019-01-24 23:24:15 +0100

    Remove cmake artefacts Set correct file/folder ownerships before building package

--- build-deb.sh
+++ build-deb.sh
@@ -1,7 +1,4 @@
 #/bin/bash
-# keep in mint 
-# LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib/x86_64-linux-gnu:/usr/local/lib/cutelyst2-plugins
-# has to be set
 
 docker build -t virtlyst .
 mkdir ./tmp
@@ -16,5 +13,11 @@ cp -a ./tmp/local/man virtlyst-deb/usr/local/
 cp -a ./tmp/local/share virtlyst-deb/usr/local/
 cp -a ./tmp/local/src/Virtlyst/root virtlyst-deb/usr/local/var/virtlyst/
 cp -a ./tmp/local/src/Virtlyst/src/libVirtlyst.so  virtlyst-deb/usr/local/lib/
+rm -fR virtlyst-deb/usr/local/lib/x86_64-linux-gnu/pkgconfig
+rm -fR virtlyst-deb/usr/local/lib/x86_64-linux-gnu/cmake
+
+chown root:root -R virtlyst-deb/usr
+
+rm -f virtlyst-deb.deb
 
 dpkg --build virtlyst-deb
