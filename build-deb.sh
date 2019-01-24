c254423c26798871ee6ec1d6c74afbb876254e32
Schnuffle <me@schnuffle.de>
2019-01-24 03:45:15 +0100

    Correct wrong paths in copy command Added path to .gitignore

--- build-deb.sh
+++ build-deb.sh
@@ -2,15 +2,15 @@
 docker build -t virtlyst .
 mkdir ./tmp
 mkdir -p virtlyst-deb/usr/local/var/virtlyst
-docker run --name virtlysttmp --rm virtlyst
+docker run --name virtlysttmp -d --rm virtlyst
 docker cp virtlysttmp:/usr/local ./tmp
 docker stop virtlysttmp
 
-cp -a ./tmp/lib virtlyst-deb/usr/local/
-cp -a ./tmp/bin virtlyst-deb/usr/local/
-cp -a ./tmp/man virtlyst-deb/usr/local/
-cp -a ./tmp/share virtlyst-deb/usr/local/
-cp -a ./tmp/src/Virtlyst/root virtlyst-deb/usr/local/var/virtlist
+cp -a ./tmp/local/lib virtlyst-deb/usr/local/
+cp -a ./tmp/local/bin virtlyst-deb/usr/local/
+cp -a ./tmp/local/man virtlyst-deb/usr/local/
+cp -a ./tmp/local/share virtlyst-deb/usr/local/
+cp -a ./tmp/local/src/Virtlyst/root virtlyst-deb/usr/local/var/virtlist
 #cp -a ~/src/Virtlyst/usr/local/src/Virtlyst/config.ini ../../etc/
-cp -a ./tmp/src/libVirtlyst.so  virtlyst-deb/usr/local/lib/
+cp -a ./tmp/local/src/Virtlyst/src/libVirtlyst.so  virtlyst-deb/usr/local/lib/
 
